87bf28d4fe1a185be61360b3c3420474735367ea
MintzyG <ericbraga2000@gmail.com>
2023-03-16 16:53:00 -0300

    GO: Made the structure for exercises better and provided the actual first exercise

---
+++ Programming Languages/GOlang/Exercises/A Tour Of Go Exercises/01: Square root function/Square root function exercise.go
@@ -0,0 +1,9 @@
+// Square root function exercise
+// Now that we learned the basics of functions and loops
+// let's try to code a simple function that calculates the square root of any given input
+// and stops when the value stops changing by big amounts
+// usually square root functions have a starting guess, try changing it and see how much faster
+// you can calculate your square root
+// Good luck!!
+
+package main
