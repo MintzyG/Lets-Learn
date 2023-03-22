a66c9ad94dfdbccaee644599a750e42e3496d1f4
MintzyG <ericbraga2000@gmail.com>
2023-03-22 17:27:38 -0300

    GO: Added Sophia's solution to 'Readers' exercise

--- Programming Languages/GOlang/Exercises/A Tour Of Go Exercises/06: Readers exercise/Readers.go
+++ Programming Languages/GOlang/Exercises/A Tour Of Go Exercises/06: Readers exercise/Readers.go
@@ -1,1 +1,17 @@
-package _6__Readers_exercise
+// Code only runs is 'A tour of go'
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (m MyReader) Read(b []byte) (int, error) {
+
+	b[0] = 65
+
+	return 1, nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
