cbe63fc13548f94cb44fcb5f85cf45bf2a07faab
MintzyG <ericbraga2000@gmail.com>
2023-03-24 16:04:47 -0300

    GO: Modified 'Goroutines' module
    Fixed it and explained it

--- Programming Languages/GOlang/Modules/19: Goroutines.go
+++ Programming Languages/GOlang/Modules/19: Goroutines.go
@@ -5,20 +5,44 @@ import (
 	"time"
 )
 
-func f(from string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(from, ":", i)
+// A goroutine is a lightweight thread of execution
+// It's generally used to execute multiple functions at the same time
+
+// Suppose we have the following function say()
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
 	}
 }
 
-//FIXME: For some reason goroutines are broken, find out later and fix it/explain it
-
 func main() {
 
-	f("Function call")
+	// This is how you normally run it synchronously(One func at a time)
+	say("hello")
 
-	go f("Goroutine")
+	// To invoke this function in a goroutine we use the keyword 'go' before it
+	// This function will be executed concurrently
+	go say("world")
 
-	time.Sleep(time.Second)
+	// You can also call a go routine from a anonymous function
+	go func(msg string) {
+		i := 0
+		fmt.Println(msg)
+		for {
+			i++
+			if i == 10 {
+				fmt.Println(msg)
+				break
+			}
+		}
+	}("going")
+
+	// Now both functions are running asynchronously in separate goroutines
+	// We have to wait for them to finish before terminating the program, so we used
+
+	time.Sleep(time.Second * 2)
+
+	// For a better approach use WaitGroups
 
 }
