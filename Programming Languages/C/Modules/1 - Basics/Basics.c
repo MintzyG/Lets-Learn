6bd2f81b5327838c481b78debb340dd9f7202b52
MintzyG <ericbraga2000@gmail.com>
2023-04-11 13:49:07 -0300

    Started C 'Variables' module

--- Programming Languages/C/Modules/1 - Basics/Basics.c
+++ Programming Languages/C/Modules/1 - Basics/Basics.c
@@ -1,5 +1,5 @@
 // Welcome to the C tutorial.
-// What i'm writing after each "//" are comments, they're ignored by the compiler and don't interfere with the code.
+// What I'm writing after each "//" are comments, they're ignored by the compiler and don't interfere with the code.
 // Use them to make your code more readable by specifying what each thing does, or even tag each code block for personal cataloguing.
 // Code can get confusing after hundreds of lines and non-linear implementation.
 
@@ -12,14 +12,15 @@
 int main() {
 	// Before beggining to code, you have to add the main function, it tells the OS when your program runs.
 	// Int is an integer value, which means it requires returning an integer to finalize the code.
-	printf ("Hello World."); // This is printf, a function that allows us to output a formatted string of characters, 
+	printf ("Hello World."); 
+    // This is printf, a function that allows us to take a string and optional arguments and produce
+    // a formatted sequence of characters for output
 	// in the example, we're inputting the standard Hello World.
 	return 0;
     // If the program returns 0, it means that it has been ran succesfully
     // Otherwise it will return a non-zero value, meaning an error.
-    // Int main is mandatory for your code to run.
+    // Int main() is mandatory for your code to run.
 }
 
-// I recommend rewriting the code yourself by memory to get accostumed 
-// to each function.
-// Use GCC or Clang compiler to compile your C into an executable file
+// I recommend rewriting the code yourself by memory to get accostumed to each function.
+// Use GCC or Clang compiler to compile your C code into an executable file
---
+++ Programming Languages/C/Modules/1 - Basics/Variables and Types.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// C, just like any other language has data types, they are used to declare the type and size of a given variable
+//
+// int myInt;
+// Here for example myInt is of the type int (used to store intergers) and has a size of 4 bytes
+//
+// C has the following data types:
+//
+//      Type                    Size
+//
+//      int                         at least 2 bytes, usually 4 bytes
+//      char                        1 byte
+//      float                       4 bytes
+//      double                      8 bytes
+//      short int                   usually 2 bytes
+//      unsigned int                at least u, usually 4 bytes
+//      long int                    at least 4, usually 8 bytes
+//      long long int               at least 8 bytes
+//      unsigned long int           at least 4 bytes
+//      unsigned long long int      at least 8 bytes
+//      signed char                 1 byte
+//      unsigned char               1 byte
+//      long double                 at least 10, usually 12 or 16 bytes
+//
+// We won't commonly use all of these, the ones we'll use most of the time are:
+// int, char, float, double
+// A more in depth explanation of each data type can be found in the 'Variables' submodule 1
+// Here only the commonly used listed will be explained
