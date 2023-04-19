43c76f066ddb5f8d48b04b1b7acbd393499f0088
MintzyG <ericbraga2000@gnail.com>
2023-04-19 16:47:28 -0300

    Modified C 3.1 'Reading' module
    Text is now easier to understand and code is more complete

--- Programming Languages/C/Modules/1 - Basics/3.1 Reading.c
+++ Programming Languages/C/Modules/1 - Basics/3.1 Reading.c
@@ -8,17 +8,21 @@ int main() {
 
     int x;
 
-    // Now that we have where to store our input lets try and get a value
-    // Scanf expects us to tell it how to format our value and where to store it
-    // For that do a simple format and use &variable to reference the variable you want to use to store your value
+    // When scanf starts it will just wait for the user to give it an input without notifiying them that its waiting
+    // So lets warn the user to input a number so they dont just stay there looking at the screen waiting for something to happen
+
+    printf("Enter a base 10 number: ");
+
+    // Now that we warned the user and have where to store our input lets try and get a value from the user
+    // Scanf expects us to tell it how to format and where to store the value thats being received
+    // For that do a simple conditional format and use &variable to reference the variable you want to use to store your value
 
     scanf("%i", &x);
 
     // Be aware that any input will be formatted to what you choose
     // So nothing prohibits a user to inpu the letter 'B' for example consequently inputing some unkown int as the value
-    
-    // Lets print our input
 
+    // Lets print our number
     printf("%i", x);
 
 }
